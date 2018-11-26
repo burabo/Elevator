0f2f62d223b0e3cfb406fb9e6193cd8aeb0608a5
burabo <brave24@protonmail.com>
2018-11-26 18:43:46 +0000

    Criação da classe Portas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Portas porta = new Portas(false);
+
+        porta.setEstado(true);
+
     }
 
 }
--- src/main/java/Portas.java
+++ src/main/java/Portas.java
@@ -1,2 +1,21 @@
 public class Portas {
+
+    /*
+     * true: aberto; false: fechado
+     */
+    boolean estado;
+
+    public Portas(){}
+
+    public Portas(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
