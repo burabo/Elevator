c13fee445dccf6911c0752bb277b4f7ed9a40cd0
burabo <brave24@protonmail.com>
2019-01-03 19:32:07 +0000

    Merge branch 'feature' of /home/gls/IdeaProjects/Elevator with conflicts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,40 @@
-import java.io.IOException;
-import java.util.concurrent.Semaphore;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
+public class Main{
+
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame("Elevador");
+        f.setSize(250, 250);
+        f.setLocation(300,200);
+        final JTextArea textArea = new JTextArea(10, 40);
+        textArea.setEditable(false);
+        f.getContentPane().add(BorderLayout.CENTER, textArea);
+        final JButton open = new JButton("<>");
+        final JButton close = new JButton("><");
+        f.getContentPane().add(BorderLayout.SOUTH, open);
+        f.getContentPane().add(BorderLayout.SOUTH, close);
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Porta aberta!\n");
+            }
+        });
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Porta fechada!\n");
+            }
+        });
+
+        f.setVisible(true);
+
+    }
 
 }
